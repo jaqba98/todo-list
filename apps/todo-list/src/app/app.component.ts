1edea78fb2074e7f6efb8ef8c4cc2cbe206df50e
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-21 12:48:36 +0100

    feat: create a default ui-controls project

--- apps/todo-list/src/app/app.component.ts
+++ apps/todo-list/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiControlsComponent } from '@todo-list/ui-controls';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, UiControlsComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ libs/ui-controls/src/lib/ui-controls/ui-controls.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui-controls',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-controls.component.html',
+  styleUrl: './ui-controls.component.css',
+})
+export class UiControlsComponent {}
