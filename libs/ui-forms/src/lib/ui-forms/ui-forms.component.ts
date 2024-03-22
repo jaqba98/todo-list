c4a898b6aae9502eb2c3dfbdb1d979941aa4eca8
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-22 10:16:13 +0100

    feat: create ui-forms project

---
+++ libs/ui-forms/src/lib/ui-forms/ui-forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-ui-forms',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ui-forms.component.html',
+  styleUrl: './ui-forms.component.css',
+})
+export class UiFormsComponent {}
