d41a2bffb5a76601c91c84af1c980c3d9ed6b423
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-25 15:03:43 +0100

    chore: add hash to the routing

--- apps/todo-list/src/app/app.config.ts
+++ apps/todo-list/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { uiPageRoutes } from '@todo-list/ui-page';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(uiPageRoutes)]
+  providers: [provideRouter(uiPageRoutes, withHashLocation())]
 };
