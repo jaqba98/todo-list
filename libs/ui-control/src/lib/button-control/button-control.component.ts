a4bbdda4ad3b60e8bcf89a909ee7b4f6a5d3a63c
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-26 14:39:04 +0100

    fix: remove unused import

--- libs/ui-control/src/lib/button-control/button-control.component.ts
+++ libs/ui-control/src/lib/button-control/button-control.component.ts
@@ -1,8 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ButtonTypeEnum } from './button-type.enum';
-
 @Component({
   selector: 'ui-control-button-control',
   standalone: true,
