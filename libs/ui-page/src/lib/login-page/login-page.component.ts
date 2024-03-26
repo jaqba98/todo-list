f63ce82d6af23d4eb9a1d9a9063676ef44aa233a
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-26 14:47:26 +0100

    feat: display login-form on the login-page

--- libs/ui-page/src/lib/login-page/login-page.component.ts
+++ libs/ui-page/src/lib/login-page/login-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginViewComponent } from '@todo-list/ui-view';
+
 @Component({
   selector: 'ui-page-login-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LoginViewComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
 })
