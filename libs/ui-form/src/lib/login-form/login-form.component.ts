f652fcb660b95723c82fb3d3ce28c6d2d814c619
Jakub Olejarczyk <jakubolejarczyk98@gmail.com>
2024-03-27 10:25:00 +0100

    feat: add login and password input to the login-form

--- libs/ui-form/src/lib/login-form/login-form.component.ts
+++ libs/ui-form/src/lib/login-form/login-form.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ButtonControlComponent } from '@todo-list/ui-control';
+import { ButtonControlComponent, InputControlComponent } from '@todo-list/ui-control';
 
 @Component({
   selector: 'ui-form-login-form',
   standalone: true,
-  imports: [CommonModule, ButtonControlComponent],
+  imports: [
+    CommonModule,
+    ButtonControlComponent,
+    InputControlComponent
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
 })
